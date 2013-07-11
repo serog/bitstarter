2d70d803a455bbd6c4686413e40f0b2ac05bc1c9
Britta Serog <britta@solutionlibre.com>
2013-07-11 23:58:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,6 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+
+ var fs = require('fs');
+  console.log('reading index.html');
+
+  var contents_buffer = fs.readFileSync('index.html');
+  var contents = contents_buffer.toString();
+  console.log(contents);
+
   response.send('Hello World 2!');
 });
 
